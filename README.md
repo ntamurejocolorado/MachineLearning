603babc9c7599c9c729980aa996b4f834788c99f
ntamurejo <noelia.tc89@gmail.com>
2022-06-17 12:42:19 +0200

    Initial commit to machine learning samples

---
+++ README.md
@@ -0,0 +1,24 @@
+# MACHINE LEARNING
+- Proyect: Machine Learning
+- Date: 2022/06
+- Responsable del proyecto: Noelia Tamurejo.
+- Desarrolladores del proyecto: Noelia Tamurejo.
+- mail contacto: noelia.tc89@gmail.com
+
+## Descripcion
+Este directorio tendrá ejemplos de redes neuronales sencillas.
+
+## Prerequisitos
+
+   - Python 3.8
+   - Statsmodels
+   - Scikit-Learn
+   - Matplotlib
+   - Pandas
+   - TensorFlow and keras
+
+## Instalación
+Entorno tox.
+    
+## Cómo usar
+
---
+++ RNN/Simple_RNN.py
@@ -0,0 +1,22 @@
+import numpy as np  
+print("Hello")
+timesteps = 5
+input_features = 3
+output_features = 2
+
+inputs = np.random.random((timesteps, input_features))
+print(inputs)
+state_t = np.zeros((output_features,))
+
+W = np.random.random((output_features, input_features))
+U = np.random.random((output_features, output_features))
+b = np.random.random((output_features,))
+
+successive_outputs = []
+
+for input_t in inputs:
+    output_t = np.tanh(np.dot(W, input_t) + np.dot(U, state_t) + b)
+
+    successive_outputs.append(output_t)
+
+final_output_sequence = np.concatenate(successive_outputs, axis=0)
